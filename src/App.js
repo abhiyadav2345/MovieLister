cef87b828913ce610bfec88efc108c2d37bf2936
abhiyadav2345 <abhiyadav2345@gmail.com>
2023-02-22 18:55:54 +0530

    error solved

--- src/App.js
+++ src/App.js
@@ -1,51 +1,52 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import searchIcon from "./search.svg";
+import React, { useState, useEffect } from "react";
+
 import MovieCard from "./MovieCard";
-// 68bfdc84
-const API_URL = "http://www.omdbapi.com/?apikey=68bfdc84";
+import SearchIcon from "./search.svg";
+import "./App.css";
 
-
+const API_URL = "http://www.omdbapi.com?apikey=68bfdc84";
 
 const App = () => {
-  const [Movies, setMovies] = useState([]);
-  const [SearchItem, setSearchItem] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
+
     setMovies(data.Search);
   };
-  useEffect(() => {searchMovies("Ironman");}, 
-  
-  []);
+
   return (
     <div className="app">
-      <h1>Flixify</h1>
+      <h1>MovieLand</h1>
+
       <div className="search">
         <input
-          value={SearchItem}
-          onChange={(e) => {
-            setSearchItem(e.target.value);
-          }}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search for movies"
         />
-
         <img
-          src={searchIcon}
-          alt="Search"
-          onClick={() => searchMovies(SearchItem)}
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
         />
       </div>
 
-      {Movies?.length > 0 ? (
+      {movies?.length > 0 ? (
         <div className="container">
-          {Movies.map((movie) => (
+          {movies.map((movie) => (
             <MovieCard movie={movie} />
           ))}
         </div>
       ) : (
-        <div>
-          <h2>No Movies Found</h2>
+        <div className="empty">
+          <h2>No movies found</h2>
         </div>
       )}
     </div>
